51cad0520999ebf693586b7325670ad323669823
Jaime Torres <jtbenavente@gmail.com>
2024-04-18 11:41:24 +0200

    ingress updated and spinner added to movies component

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MovieService } from '../../services/movie.service';
 import { BehaviorSubject } from 'rxjs';
 import { MovieComponent } from './movie/movie.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import TVEvent from '../../models/TVEvent';
 
 @Component({
@@ -10,16 +11,18 @@ import TVEvent from '../../models/TVEvent';
   standalone: true,
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css',
-  imports: [CommonModule, MovieComponent],
+  imports: [CommonModule, MovieComponent, MatProgressSpinnerModule],
 })
 export class MoviesComponent implements OnInit {
   liveMovies$ = new BehaviorSubject<TVEvent[]>([]);
+  isLoading$ = new BehaviorSubject(true);
 
   constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
-    this.movieService
-      .getLiveMovies$()
-      .subscribe((movies) => this.liveMovies$.next(movies));
+    this.movieService.getLiveMovies$().subscribe((movies) => {
+      this.liveMovies$.next(movies);
+      this.isLoading$.next(false);
+    });
   }
 }
